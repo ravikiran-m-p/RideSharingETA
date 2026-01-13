0d5e11273dea0e1ecc7fca1009d8b416b4e4b86b
MODALAVALASA RAVI KIRAN <ravikiranmodalavalasa@gmail.com>
2026-01-13 17:46:11 +0530

    Add Edge class for ride sharing ETA calculations
    Class representing a road between two locations

--- src/RideSharingETA.java
+++ src/RideSharingETA.java
@@ -1,3 +1,13 @@
+class Edge {
+    String destination;
+    int travelTime;
+
+    Edge(String destination, int travelTime) {
+        this.destination = destination;
+        this.travelTime = travelTime;
+    }
+}
+
 class RideSharingETA
 {
   public static void mian(String[] args)throwa InterruptedException
